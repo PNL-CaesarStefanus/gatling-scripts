ff675d7e35b6de88ea522c115a6cf678856a1ff8
Caesar Stefanus <caesar.stefanus@playnext-lab.co.jp>
2020-06-09 19:15:02 +0900

    no broker (test default broker)

--- src/test/scala/mqttfrontline.scala
+++ src/test/scala/mqttfrontline.scala
@@ -4,10 +4,10 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.mqtt.Predef._
 
-class MqttSample extends Simulation{
+class MqttSample extends Simulation {
 
   private val mqttConf = mqtt
-    .broker("172.31.20.201", 3700)
+    // .broker("172.31.20.201", 3700)
     .clientId("test")
     .qosAtLeastOnce
     // .correlateBy(jsonPath("$.correlationId"))
